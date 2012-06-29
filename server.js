10e04739e7e0e6add7b8ed0e5aa57e0db1f64ec7
Jesse Hattabaugh <arkanciscan@gmail.com>
2012-06-28 23:36:20 -0700

    adding RedisToGo as a memory cache

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
+var app = require('express').createServer(),
+	io = require('socket.io').listen(app),
+	redis = require("redis");
+	
+if(process.env.REDISTOGO_URL){
+	var rtg = require('url').parse(process.env.REDISTOGO_URL);
+	var cache = redis.createClient(rtg.port, rtg.hostname).
+		auth(rtg.auth.split(":")[1]);
+} else {
+	var cache = redis.createClient();
+}	
+
+cache.on("error", function (err) {
+	console.log("Redis Error: " + err);
+});
 
 app.listen(process.env.PORT);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
+  cache.incr("counts:index:requests");
 });
 
 app.get('/client.js', function (req, res) {
   res.sendfile(__dirname + '/client.js');
 });
 
-var players = [];
-
 io.sockets.on('connection', function (socket) {
-
+	
+	players = cache.incr("counts:sockets:players");
+	
     socket.emit('welcome', players);
     
     socket.broadcast.emit('player joined');
@@ -22,7 +36,8 @@ io.sockets.on('connection', function (socket) {
     console.log("New connection started");
     
     socket.on('disconnect', function(){
-        console.log("Connection stopped");
+		players = cache.decr("counts:sockets:players");
+        console.log("Connection stopped. "+players+" players remaining.");
         io.sockets.emit('player left');
     });
     
